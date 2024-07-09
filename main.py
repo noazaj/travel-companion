80d14fca6a8e22e43da85539c1e06efe3b3dd6dc
noazaj <noahzajicek@gmail.com>
2024-07-09 17:38:30 -0400

    Lint workflow works. Test flake8 styling now

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def home():
     return 'Welcome to the home page!'
-    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,12 @@
 import pytest
 from main import app
 
+
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
+
 
 def test_home(client):
     response = client.get('/')
