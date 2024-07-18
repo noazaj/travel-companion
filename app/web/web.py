1cf45d0ad0a40f56d10b50c1745b16108f83b096
noazaj <noahzajicek@gmail.com>
2024-07-18 00:48:36 -0400

    Create blueprint to expand routes for template html files

---
+++ app/web/web.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+web_bp = Blueprint('web', __name__, static_folder='../web/static', template_folder='templates')
+
+@web_bp.route('/', methods=['GET'])
+def home():
+    try:
+        return render_template('index.html')
+    except TemplateNotFound:
+        abort(404)
+
+@web_bp.route('/plan-a-trip', methods=['GET'])
+def plan_a_trip():
+    try:
+        return render_template('plan-a-trip.html')
+    except TemplateNotFound:
+        abort(404)
+
+# Resource(s) Used:
+# https://flask.palletsprojects.com/en/3.0.x/blueprints/
