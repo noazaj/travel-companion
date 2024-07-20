e0be76b4d700f5eb0ef4d2c59e0bf1446acd612e
Kongkom Hiranpradit <hiranprk@oregonstate.edu>
2024-07-21 01:10:28 +0700

    Server can now connect to GPT. POST request to localhost:8080 to initiate trip planning. PATCH request to chat with GPT there after. Sample requests are in the postman files.

--- main.py
+++ main.py
@@ -23,7 +23,8 @@ load_dotenv()
 # This line brings all environment variables from .env into os.environ
 client = OpenAI()
 # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
-# if you saved the key under a different environment variable name, you can do something like:
+# if you saved the key under a different environment variable name, 
+# you can do something like:
 # client = OpenAI(
 #   api_key=os.environ.get("CUSTOM_ENV_NAME"),
 # )
