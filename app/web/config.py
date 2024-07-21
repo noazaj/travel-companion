309fa4508411afda90288793682678ce045ba358
noazaj <noahzajicek@gmail.com>
2024-07-21 01:14:39 -0400

    Add Facebook register configs and OAuth method

--- app/web/config.py
+++ app/web/config.py
@@ -20,6 +20,29 @@ class Config:
             'api_base_url': 'https://api.github.com/',
             'client_kwargs': {'scope': 'user:email'},
         },
+        
+        # Google OAuth 2.0 documentation:
+        # https://developers.google.com/identity/protocols/oauth2
+        'google': {
+            'client_id': os.getenv('GOOGLE_CLIENT_ID'),
+            'client_secret': os.getenv('GOOGLE_CLIENT_SECRET'),
+            'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+            'token_url': 'https://accounts.google.com/o/oauth2/token',
+            'api_base_url': 'https://www.googleapis.com/oauth2/v3/userinfo',
+            'client_kwargs': {'scope': 'openid profile email'},
+            'jwks_uri': 'https://www.googleapis.com/oauth2/v3/certs',
+        },
+        
+        # Facebook OAuth 2.0 documentation:
+        # https://developers.facebook.com/docs/facebook-login/guides/advanced/manual-flow/
+        'facebook': {
+            'client_id': os.getenv('FACEBOOK_CLIENT_ID'),
+            'client_secret': os.getenv('FACEBOOK_CLIENT_SECRET'),
+            'authorize_url': 'https://www.facebook.com/v20.0/dialog/oauth?',
+            'token_url': 'https://graph.facebook.com/v20.0/oauth/access_token?',
+            'api_base_url': 'https://graph.facebook.com/',
+            'client_kwargs': {'scope': 'email'}
+        },
     }
 
 
